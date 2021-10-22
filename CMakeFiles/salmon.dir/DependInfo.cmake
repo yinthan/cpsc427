8b175487b66c93645b70928b3ac0399bd43be1fa
yinthan <44821745+yinthan@users.noreply.github.com>
2021-10-22 00:37:13 -0700

    Add files via upload

---
+++ CMakeFiles/salmon.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/ai_system.cpp" "CMakeFiles/salmon.dir/src/ai_system.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/ai_system.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/common.cpp" "CMakeFiles/salmon.dir/src/common.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/common.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/components.cpp" "CMakeFiles/salmon.dir/src/components.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/components.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/main.cpp" "CMakeFiles/salmon.dir/src/main.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/main.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/physics_system.cpp" "CMakeFiles/salmon.dir/src/physics_system.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/physics_system.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/render_system.cpp" "CMakeFiles/salmon.dir/src/render_system.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/render_system.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/render_system_init.cpp" "CMakeFiles/salmon.dir/src/render_system_init.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/render_system_init.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/tiny_ecs.cpp" "CMakeFiles/salmon.dir/src/tiny_ecs.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/tiny_ecs.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/tiny_ecs_registry.cpp" "CMakeFiles/salmon.dir/src/tiny_ecs_registry.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/tiny_ecs_registry.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/world_init.cpp" "CMakeFiles/salmon.dir/src/world_init.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/world_init.cpp.o.d"
+  "/Users/ethan/Desktop/CPSC427/template_v2/src/world_system.cpp" "CMakeFiles/salmon.dir/src/world_system.cpp.o" "gcc" "CMakeFiles/salmon.dir/src/world_system.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
